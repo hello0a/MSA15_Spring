30dc11b5ad6a771898a7758cd5d5ca57f5b7165f
hello0a <aya70713@gmail.com>
2026-01-09 12:19:10 +0900

    게시판 만들기(css 적용)

---
+++ board/src/main/java/com/aloha/board/controller/HomeController.java
@@ -0,0 +1,19 @@
+package com.aloha.board.controller;
+
+import org.springframework.stereotype.Controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Slf4j
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        log.info("메인 화면");
+        return "index";
+    }
+    
+}
